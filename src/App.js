af289d9b0b308904e45ce27386813c39ad755829
bhagyashree-ghude <bhagyashreeghude@gmail.com>
2024-04-13 13:51:57 +0530

    clear all mess

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-// import logo from './logo.svg';
 import { useState } from 'react';
-import './App.css';
 
 function App() {
-  const [ data,setData] =useState("")
-  return (
-    <div className="App">
-      <h1>123456 First React Test case </h1>
-
-      <input type='text' placeholder='user name' name='user-name' id='user-id'/>
-      <img  title='nature-img' src='https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072821_640.jpg' alt='img'></img>
-   
-      <h1> Testing onChange Event with Input Text</h1>
-      {/* <input type='text' value={data} onChange={(e)=>setData(e.target.value)}/> */}
-      
-      <button onClick={()=>setData("update Data")}>Update data</button>
-      <h1>jest testing</h1>
-      <h2>hello testing</h2>
-      <h1>{data}</h1>
-    </div>
-  );
+  const [data,setData] = useState("")
+  const handlebtn = () =>{
+      setData("hello")
+  }
+  return(
+      <div>
+          <h1> funfile component</h1>
+          <button data-testid="btn1" onClick={handlebtn}>button</button>
+          <h4>{data}</h4>
+      </div>
+  )
 }
-
 export default App;
--- src/__tests__/snapshot.js
+++
@@ -1,7 +0,0 @@
-import { render } from "@testing-library/react";
-import App from "../App";
-
-test("snapshot testing",()=>{
-  const view= render(<App/>);
-    expect (view).toMatchSnapshot()
-})
